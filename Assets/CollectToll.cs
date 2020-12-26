a8a19042fb148729e156fa2c9adf8d23e4f0fcaf
John McDonald <jmcdonald1991@gmail.com>
2020-12-26 06:04:49 -0600

    initial commit

---
+++ Assets/CollectToll.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectToll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print("collided");
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        print(collider.tag);
+        print("trigger");
+        SendMessage("OnEnterTollbooth");
+    }
+}
